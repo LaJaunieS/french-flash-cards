853a86e9d07b57927c0171ebfb370f78d905626f
slajaunie <stephanlajaunie@yahoo.com>
2017-03-29 14:44:13 -0700

    refactor for module

--- json/concat.py
+++ json/concat.py
@@ -1,31 +1,37 @@
 import glob
 
-#grab all jsons with glob
-#loop over and open all jsons with json
-#concat
-#print to a new file
-
-my_jsons = glob.glob("src/*.json")
-agg_file = open("master/vocab_agg.json", 'w')
-print("file mode",agg_file.mode)
+# grab all jsons with glob
+# loop over and open all jsons with json
+# concat
+# print to a new file
 
 
-aggregate = "["
-for json in my_jsons:
-	f = open(json)
-	with open(json) as f:
-		aggregate += f.read()
-		aggregate += ","
-	f.close()
+class Aggregate:
+	my_jsons = glob.glob("src/*.json")
+	agg_file = open("master/vocab_agg.json", 'w')
+
+	def create_aggregate(self):
+		self.aggregate = "["
+
+		for json in self.my_jsons:
+			f = open(json)
+			with open(json) as f:
+				self.aggregate += f.read()
+				self.aggregate += ","
+			f.close()
+		self.aggregate += "]"
+		# minifies output
+		self.aggregate = "".join(self.aggregate.split("\n"))
+
+	def handle_file(self):
+		self.agg_file.write(self.aggregate)
+		self.agg_file.close()
+
+	def __init__(self):
+		self.create_aggregate()
+		self.handle_file()
+		print("file mode", self.agg_file.mode)
+		print("aggregate json file printed")
 
 
-aggregate += "]"
-
-
-# minifies output
-aggregate = "".join(aggregate.split("\n"))
-
-agg_file.write(aggregate)
-agg_file.close()
-print("aggregate json file printed")
-#print(aggregate)
+Aggregate()
