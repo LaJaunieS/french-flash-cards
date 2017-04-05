f94e7b5a45315afd2a46f8e115dc60fcc6c7ba16
slajaunie <stephanlajaunie@yahoo.com>
2017-04-05 09:58:39 -0700

    begin reworking js file org

---
+++ js/app.js
@@ -0,0 +1,228 @@
+
+   let randomInt = function(min, max) {
+                      min = Math.ceil(min);
+                      max = Math.floor(max);
+                      return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+
+
+(function(angular) {
+    
+  var app = angular.module("francaisApp", ['ngSanitize', 'ngAnimate', 'ngAria'])
+
+      .controller("flashCardCtrl", [ "$scope", "$timeout", "$http", function($scope, $timeout, $http) {
+
+
+        //this object will hold all of the data contained in the separate json files- populated completely on page load
+        $scope.sectionsObject  = {
+        "menuArray": []  //used to display length of section on main menu view
+        };
+
+        $scope.thisSection = {};
+
+
+        $scope.getJson = function() {
+            $http.get('./json/master/vocab_agg.json').then(
+                    function successCallback(data) {
+                        console.log(data.data);
+                        for (let z = 0; z < data.data.length; z++ ) {
+                            //console.log(data.data[z]);
+                            $scope.sectionsObject[data.data[z].type] = data.data[z];
+                            $scope.sectionsObject.menuArray.push( { "source": data.data[z], "type": data.data[z].type, "count": data.data[z].vocabArray.length} )
+                        };
+                            console.log('json data request successful');
+                            //console.log($scope.sectionsObject);
+                    }, function errorCallback() {
+                        document.getElementById('main-menu-wrapper').innerHTML = '<h2>error connecting to data. try reloading</h2>';
+                    }); 
+
+            };
+
+        //pull the data from json file
+        $scope.getJson();
+
+          
+         
+        //now that json data loaded, use it to show list of section items and populate a local variable when item clicked
+        //initial values- populated when section clicked  
+        $scope.section;   
+        $scope.thisSection = {
+                                "source": {},
+                                "vocab": [{ }]
+                                };
+          
+                  
+        $scope.randomNum = randomInt(0, $scope.thisSection.vocab.length-1 );
+        $scope.randomWord = $scope.thisSection.vocab[$scope.randomNum];
+        $scope.language = 1;  //1=anglais, 0=francais
+        $scope.definitions = $scope.randomWord.definitions; 
+        $scope.descriptions = [];
+                      
+        $scope.getNewWord = function(dbArray) {
+            $scope.descriptions = [];
+            $scope.randomNum = randomInt(0, dbArray.length-1);
+            $scope.randomWord = dbArray[$scope.randomNum];
+            $scope.definitions = $scope.randomWord.definitions;
+            };   
+          
+        // $scope.setTitle = function(db) {
+        //     $scope.section = db.type;  //eg $scope.sectionsObject.verbs - see templates flashCard.html for where this is interpolated- used as "title" of flash card section
+        //     };
+          
+        $scope.startSection = function(section) {
+            //hide all elements for ui effect
+            $scope.hideElement('main-menu-content-fade');
+            $scope.hideElement('flash-card-content-wrapper');
+            $scope.hideElement('card-front-content-fade');
+            $scope.hideElement('card-back-content-fade');
+            //re-add flash card wrapper to DOM
+            document.getElementById('flash-card-content-wrapper').style.display= 'block';
+            //remove menu wrapper from DOM (sub-wrapper as well since absolute positioned)
+            document.getElementById('main-menu-content-fade').style.display= 'none';
+            document.getElementById('main-menu-wrapper').style.display="none";
+            
+            $scope.thisSection = {
+                "source": $scope.sectionsObject[section],
+                "vocab": $scope.sectionsObject[section].vocabArray,
+                "title": $scope.sectionsObject[section].fullTitle
+            };
+          	
+        	
+            $scope.switchToEnglish();
+            $timeout(function() {
+                    $scope.getNewWord($scope.thisSection.vocab);
+                    //$scope.setTitle($scope.thisSection); //now section you are currently in displayed in flashCard template
+                    $scope.language = 0;
+                    $scope.switchLanguage();
+                    $scope.showElement('flash-card-content-wrapper');
+                    $scope.showElement('card-front-content-fade');
+                    }, 500);
+            };
+          
+        $scope.showElement = function(element) {
+            document.getElementById(element).style.opacity='1.0';
+            };
+        $scope.hideElement = function(element) {
+              document.getElementById(element).style.opacity='0.0';
+              };
+                           
+        $scope.switchToEnglish = function() {
+            document.getElementById('showButton').textContent = "fr";
+            $scope.language = 1;
+        };
+        $scope.switchToFrench = function() {
+            document.getElementById('showButton').textContent = "en";
+                $scope.language = 0;
+        };
+        $scope.toggleTitle = function() {
+            if ($scope.titleCondition=== true) {
+                document.getElementById('main-menu-content-fade').firstElementChild.textContent = 'french flash cards';
+                $scope.titleCondition = false;
+                } else {
+                    document.getElementById('main-menu-content-fade').firstElementChild.innerHTML = 'cartes flash fran&#231;ais';
+                    $scope.titleCondition = true;
+                };
+        };
+                 
+        $scope.backToMenu = function() {
+            $scope.hideElement('flash-card-content-wrapper');
+            $scope.showElement('main-menu-content-fade');
+            $scope.about = false;
+            $timeout(function() {
+                document.getElementById('main-menu-content-fade').style.display= 'block';
+                document.getElementById('main-menu-wrapper').style.display= 'block';
+                    }, 400);
+            $timeout(function() {
+                    document.getElementById('flash-card-content-wrapper').style.display= 'none';
+                    }, 400);
+               }  
+          
+        $scope.switchLanguage = function() {
+            if ($scope.language === 1) {
+                $scope.switchToFrench();
+                $scope.hideElement('card-front-content-fade');
+                $scope.showElement('card-back-content-fade');
+                } else {
+                $scope.switchToEnglish();
+                $scope.hideElement('card-back-content-fade');
+                $scope.showElement('card-front-content-fade');    
+                 };
+        };
+          
+        $scope.nextWord = function() {
+            
+            //hide both sides for ui effect
+            $scope.hideElement('card-front-content-fade');
+            $scope.hideElement('card-back-content-fade');
+            //get a new word after both sides faded
+            $timeout(function() {
+                    $scope.getNewWord($scope.thisSection.vocab);
+                    //show front of card by default
+                    $scope.showElement('card-front-content-fade');
+                    //if in french view switch back to english
+                    if ($scope.language === 0) {
+                        $scope.switchToEnglish();
+                    };                    
+                }, 500);
+        };
+         
+    
+       
+    
+    }])
+    .directive('mainMenu', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "templates/mainMenu.html"
+            };
+    })
+    .directive('flashCard', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "templates/flashCard.html",
+            
+            };
+        
+    })
+    .directive('cardFront', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "templates/cardFront.html"            
+        };       
+    })
+    .directive('cardBack', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "templates/cardBack.html"            
+        };       
+    })
+    .directive('about', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "templates/about.html"
+        };
+    });
+    
+    
+})(window.angular);
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
